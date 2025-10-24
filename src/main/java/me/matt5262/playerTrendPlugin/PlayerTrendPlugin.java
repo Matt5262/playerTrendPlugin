3eef6300fa642d7bcd6d5fff72938f76076943d2
Matti <mattibruun2000@gmail.com>
2025-10-24 21:09:59 +0200

    third commit

---
+++ src/main/java/me/matt5262/playerTrendPlugin/PlayerTrendPlugin.java
@@ -0,0 +1,17 @@
+package me.matt5262.playerTrendPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerTrendPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
